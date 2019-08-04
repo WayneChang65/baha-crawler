6e9036b10f0f92c00f94d43a594924df07fb4f4f
Wayne_Mac <wayne65@gmail.com>
2019-08-04 14:45:07 +0800

    chore(eslint): Add npm scrpit 'lint' and fix problems

--- test/baha_crawler.test.js
+++ test/baha_crawler.test.js
@@ -1,23 +1,23 @@
 const baha_crawler = require('../index.js');
 
 test('1. Test for default Options ', async () => {
-    await baha_crawler.initialize();
-    let baha = await baha_crawler.getResults(); // Default Options
-    await baha_crawler.close();
-    expect(baha.titles).toBeDefined();
-    expect(baha.urls).toBeDefined();
+	await baha_crawler.initialize();
+	let baha = await baha_crawler.getResults(); // Default Options
+	await baha_crawler.close();
+	expect(baha.titles).toBeDefined();
+	expect(baha.urls).toBeDefined();
 }, 20000);  // 20 seconds
 
 test('2. Test for scraping "PokemonGo" board with 2 pages and containing contents of posts ' + 
-    'by skipping bottom fixed posts. ', async () => {
-    await baha_crawler.initialize();
-    let baha = await baha_crawler.getResults({
-        board: '23805',
-        pages: 3,
-        skipTPs: true
-    }); // scraping "ToS" board, 3 pages, skip fixed upper posts
-    
-    await baha_crawler.close();
-    expect(baha.titles).toBeDefined();
-    expect(baha.urls).toBeDefined();
+	'by skipping bottom fixed posts. ', async () => {
+	await baha_crawler.initialize();
+	let baha = await baha_crawler.getResults({
+		board: '23805',
+		pages: 3,
+		skipTPs: true
+	}); // scraping "ToS" board, 3 pages, skip fixed upper posts
+	
+	await baha_crawler.close();
+	expect(baha.titles).toBeDefined();
+	expect(baha.urls).toBeDefined();
 }, 180000); // 3 minutes
